fb4ec7f9f1683ea3e6d012e90e0b2868d14d3ea0
yulia <oleksunyulia30.08@gmail.com>
2026-05-21 20:07:10 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://nfwoaefeeottfjhuonko.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5md29hZWZlZW90dGZqaHVvbmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1OTg5MTYsImV4cCI6MjA5NDE3NDkxNn0.KyCZTOWFj296_huPR5Pgp1_T-bJuWswp1DBadiopUwk";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
